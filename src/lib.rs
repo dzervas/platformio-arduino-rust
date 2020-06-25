d1ca56584efa5d594bc59d2078baca7cfa179efb
Dimitris Zervas <dzervas@dzervas.gr>
2020-06-25 21:40:05 +0300

    Simplified the setup a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,13 @@
-// #![feature(start)]
 #![feature(lang_items)]
 #![no_std]
-// #![no_main]
+#![allow(dead_code)]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
-// extern crate panic_halt;
-
-#[allow(dead_code)]
 mod pio_rust;
 
-// #[link(name = "FrameworkArduino")]
 include!("platformio.rs");
-// mod platformio;
-// use crate::platformio::{pinMode, digitalWrite};
-
-// extern {
-// fn digitalWrite(ulPin: u32, ulVal: u32);
-// fn pinMode(ulPin: u32, ulMode: u32);
-// }
-
-// #[no_mangle]
-// pub extern "C" fn _start() {
-    // setup();
-// }
 
 #[no_mangle]
 pub extern "C" fn setup() {
@@ -42,15 +25,6 @@ pub extern "C" fn r#loop() {
         delay(1000);
     }
 }
-
-// #[no_mangle]
-// pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    // setup();
-
-    // loop {
-        // r#loop();
-    // }
-// }
 
 #[panic_handler]
 fn my_panic(_info: &core::panic::PanicInfo) -> ! {
