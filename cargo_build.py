21c584e54285fe43bc385b94027b4e9211847ee9
Dimitris Zervas <dzervas@dzervas.gr>
2020-06-30 04:46:49 +0300

    Implemented all of the binding generation in the pio buildscript - simplifies the whole process a lot and allow for proper IDE completion

--- cargo_build.py
+++ cargo_build.py
@@ -1,45 +1,140 @@
 import os
-from os.path import join
+from configparser import ConfigParser, NoOptionError
 from glob import glob
+from os import popen
+from os.path import join, isfile
 from pathlib import Path
 from SCons.Script import Builder
 
 # PlatformIO specific
 Import("env")
 
+MOD_PRELUDE = """
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+pub type c_schar = i8;
+pub type c_uchar = u8;
+pub type c_char = i8;
+pub type c_short = i16;
+pub type c_ushort = u16;
+pub type c_int = i16;
+pub type c_uint = u16;
+pub type c_float = f32;
+pub type c_double = f64;
+pub type c_long = i32;
+pub type c_ulong = u32;
+pub type c_longlong = i64;
+pub type c_ulonglong = u64;
+pub type intmax_t = i64;
+pub type uintmax_t = u64;
+
+pub type size_t = usize;
+pub type ptrdiff_t = isize;
+pub type intptr_t = isize;
+pub type uintptr_t = usize;
+pub type ssize_t = isize;
+
+pub type c_void = core::ffi::c_void;
+"""
+
+def get_rust_headers():
+	config = ConfigParser()
+	config.read(env.subst("$PROJECT_CONFIG"))
+
+	try:
+		headers_to_process_str = config.get(env.subst("env:$PIOENV"), "rust_headers")
+	except NoOptionError:
+		return []
+
+	if isinstance(headers_to_process_str, list):
+		headers_to_process_str = headers_to_process_str[0]
+
+	if not headers_to_process_str:
+		return []
+
+	headers_to_process = headers_to_process_str.split("\n")
+
+	if len(headers_to_process) <= 1:
+		headers_to_process = headers_to_process_str.split(",")
+
+	if not headers_to_process:
+		return []
+
+	headers_to_process = [x.strip() for x in headers_to_process]
+
+	print("Searching headers to generate bindings:", headers_to_process)
+
+	headers_found = {}
+
+	for header in headers_to_process:
+		found = False
+
+		for inc_path in env.get("CPPPATH"):
+			header_path = inc_path + "/" + header
+
+			if isfile(header_path):
+				name = header.replace("/", "_")
+				name = name.replace(".", "_")
+				name += ".rs"
+
+				headers_found[name] = header_path
+				found = True
+				break
+
+		if not found:
+			print("Unable to find header", header)
+
+	print("Found headers:", headers_found)
+	return headers_found
+
 def ignore_main_cpp(node):
-	headers = env.subst(env.get("_CPPINCFLAGS"))
+	defines = env.subst("$_CPPDEFFLAGS").replace("\\\"", "\"")
+	sysroot = popen(env.subst("$CC -print-sysroot")).read().strip()
+	mod_rs = open(env.subst("$PROJECT_SRC_DIR/platformio/mod.rs"), "w")
+	mod_rs.write(MOD_PRELUDE)
 
-	toolchain_dir = env.PioPlatform().get_package_dir("toolchain-gccarmnoneeabi")
-	toolchain_headers_path = join(toolchain_dir, "lib/gcc/arm-none-eabi/*/include")
-	toolchain_headers_paths = [str(Path(p)) for p in glob(toolchain_headers_path)]
-	toolchain_headers = " -I" + " -I".join(toolchain_headers_paths)
+	env.Execute("mkdir -p $PROJECT_SRC_DIR/platformio")
 
-	headers += toolchain_headers + " "
+	for name, h in get_rust_headers().items():
+		# -target should reflect cargo's target. Ex. thumbv7m-none-eabi becomes armv7m while thumbv7m-none-eabihf becomes armv7em
+		# Floating point does not make a difference in target in clang, but it does in -mfloat-abi
+		# -mfloat-abi=soft for software and hard for hardware. There's also softfp (?)
+		# I have no idea why the nrf52, while being hard requires soft ABI. Gotta check that at some point
+		env.Execute("""bindgen --ctypes-prefix super --use-core \
+					   --blacklist-item std::* \
+					   --blacklist-item FP_NAN \
+					   --blacklist-item FP_INFINITE \
+					   --blacklist-item FP_ZERO \
+					   --blacklist-item FP_NORMAL \
+					   --blacklist-item FP_SUBNORMAL \
+					   --blacklist-item SVCALL \
+					   --blacklist-function setup \
+					   --blacklist-function loop \
+					   -o $PROJECT_SRC_DIR/platformio/{name} \
+					   {header} -- \
+					   -DCMAKE_CROSSCOMPILING=True \
+					   -DLLVM_DEFAULT_TARGET_TRIPLE={target} \
+					   -DCMAKE_CXX_FLAGS="$CCFLAGS $CXXFLAGS" \
+					   {defines} $_CPPINCFLAGS \
+					   --sysroot "{sysroot}" \
+					   -mfloat-abi=soft \
+					   -target {target}""" .format(
+						   name=name,
+						   header=h,
+						   sysroot=sysroot,
+						   target="thumbv7em-none-eabihf",
+						   defines=defines))
 
-	defines = env.subst(env.get("_CPPDEFFLAGS")).replace("\\\"", "\"")
+		# As the preprocessor does not have any idea abut namespaces, we can re-export all the functions safely
+		mod_rs.write("""
+pub mod {name};
+pub use {name}::*;
+		""".format(name=name[:-3]))  # Remove .rs extension
 
-	# -target should reflect cargo's target. Ex. thumbv7m-none-eabi becomes armv7m while thumbv7m-none-eabihf becomes armv7em
-	# Floating point does not make a difference in target in clang, but it does in -mfloat-abi
-	# -mfloat-abi=soft for software and hard for hardware. There's also softfp (?)
-	# I have no idea why the nrf52, while being hard requires soft ABI. Gotta check that at some point
-	# env.Execute("""bindgen --ctypes-prefix pio_rust --use-core \
-	# 			   --blacklist-item std::* \
-	# 			   --blacklist-item FP_NAN \
-	# 			   --blacklist-item FP_INFINITE \
-	# 			   --blacklist-item FP_ZERO \
-	# 			   --blacklist-item FP_NORMAL \
-	# 			   --blacklist-item FP_SUBNORMAL \
-	# 			   --blacklist-item SVCALL \
-	# 			   --blacklist-function setup \
-	# 			   --blacklist-function loop \
-	# 			   -o src/platformio.rs \
-	# 			   """ + env.get("PROJECT_PACKAGES_DIR") +"""/framework-arduinoadafruitnrf52/cores/nRF5/Arduino.h -- \
-	# 			   -target armv7em """
-	# 			   + defines + " " + headers)
-
-	print(dir(env.get("ENV")))
-	env.get("ENV")["CPPPATH"] = ":".join(env.get("CPPPATH"))
+	mod_rs.close()
 	env.Execute("cargo build --release --target=thumbv7em-none-eabihf -v")
 	env.Append(PIOBUILDFILES=["$PROJECT_DIR/target/thumbv7em-none-eabihf/release/deps/firmware-platformio.o"])
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,10 @@
-#![feature(lang_items)]
 #![no_std]
-#![allow(non_upper_case_globals)]
-#![allow(non_snake_case)]
-#![allow(non_camel_case_types)]
 
-#[allow(dead_code)]
-mod pio_rust;
+mod platformio;
 
-extern crate pio_include;
-use pio_include::pio_include;
-pio_include!("Arduino.h");
-
-// include!("platformio.rs");
+use platformio::{pinMode, digitalWrite, delay};
 
 #[no_mangle]
-#[allow(dead_code)]
 pub extern "C" fn setup() {
     unsafe {
         pinMode(19, 1);
@@ -22,7 +12,6 @@ pub extern "C" fn setup() {
 }
 
 #[no_mangle]
-#[allow(dead_code)]
 pub extern "C" fn r#loop() {
     unsafe {
         digitalWrite(19, 1);
@@ -36,6 +25,3 @@ pub extern "C" fn r#loop() {
 fn my_panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
-
-#[lang = "eh_personality"]
-fn eh_personality() {}
